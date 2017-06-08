d633f5f060180b5839902bc191de255d929accd4
Tomas Krizek <tkrizek@redhat.com>
2017-06-08 13:46:55 +0200

    tasks: add failure_severity to Popen task
    Add support for different log levels for stderr of Popen.
    Additionally, log level ERROR and above will raise an exception.
    
    Signed-off-by: Tomas Krizek <tkrizek@redhat.com>

--- tasks/task.py
+++ tasks/task.py
@@ -29,16 +29,23 @@ class Task(object):
 
 
 class PopenTask(Task):
-    def __init__(self, cmd):
+    def __init__(self, cmd, failure_severity=logging.ERROR):
         super(PopenTask, self).__init__()
         self.cmd = cmd
+        self.loglvl = failure_severity
 
     def _target(self):
-        self.process = subprocess.Popen(self.cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        self.process = subprocess.Popen(self.cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         for line in iter(self.process.stdout.readline, b''):
             logging.debug(line.rstrip('\n'))
+        for line in iter(self.process.stderr.readline, b''):
+            logging.log(self.loglvl, line)
         self.process.wait()
         self.returncode = self.process.returncode
+        if self.loglvl >= logging.ERROR and self.returncode != 0:
+            raise TaskException(
+                'Process exited with error code: {returncode}'.format(
+                    returncode=self.returncode))
 
 
 class TaskException(Exception):
